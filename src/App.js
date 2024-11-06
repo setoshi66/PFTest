8283d6a0547da163dc26145b5898eb5094327d5d
setoshi66 <sealedorleft24@yahoo.co.jp>
2024-11-06 16:44:06 +0900

    app test

--- src/App.js
+++ src/App.js
@@ -1,24 +1,23 @@
-import logo from './logo.svg';
-import './App.css';
+import React from 'react';
+// import { BrowserRouter, Route, Routes } from 'react-router-dom';
+// import Login from './Login';
+// import MainMenu from './MainMenu';
+// import Lists from './Lists';
 
-function App() {
+const App = () => {
   return (
-    <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
-    </div>
+    <div>あああああああ</div>
+    // <BrowserRouter>
+    //   <Routes>
+    //     <Route path="/" component={Login} />
+    //     {/* <Route path="/main" component={MainMenu} /> */}
+    //     <Route path="" element={<Login/>} />
+    //     <Route path="/" element={<Login/>} />
+    //     <Route path="/login" element={<Login/>} />
+    //     <Route path="/main" element={<MainMenu/>} />
+    //     <Route path="/lists" element={<Lists/>} />
+    //   </Routes>
+    // </BrowserRouter>
   );
 }
 
