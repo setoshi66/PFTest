a743d6e1f94fb439c262dd8ab8b0e2db4868c9e9
setoshi66 <sealedorleft24@yahoo.co.jp>
2024-11-07 11:57:57 +0900

    修正&パッケージ追加

--- src/App.js
+++ src/App.js
@@ -1,23 +1,23 @@
 import React from 'react';
-// import { BrowserRouter, Route, Routes } from 'react-router-dom';
-// import Login from './Login';
-// import MainMenu from './MainMenu';
-// import Lists from './Lists';
+import { BrowserRouter, Route, Routes } from 'react-router-dom';
+import Login from './Login';
+import MainMenu from './MainMenu';
+import Lists from './Lists';
 
 const App = () => {
   return (
-    <div>あああああああ</div>
-    // <BrowserRouter>
-    //   <Routes>
-    //     <Route path="/" component={Login} />
-    //     {/* <Route path="/main" component={MainMenu} /> */}
-    //     <Route path="" element={<Login/>} />
-    //     <Route path="/" element={<Login/>} />
-    //     <Route path="/login" element={<Login/>} />
-    //     <Route path="/main" element={<MainMenu/>} />
-    //     <Route path="/lists" element={<Lists/>} />
-    //   </Routes>
-    // </BrowserRouter>
+    // <div>！！！</div>
+    <BrowserRouter>
+      <Routes>
+        <Route path="/" component={Login} />
+        {/* <Route path="/main" component={MainMenu} /> */}
+        <Route path="" element={<Login/>} />
+        <Route path="/" element={<Login/>} />
+        <Route path="/login" element={<Login/>} />
+        <Route path="/main" element={<MainMenu/>} />
+        <Route path="/lists" element={<Lists/>} />
+      </Routes>
+    </BrowserRouter>
     // import logo from './logo.svg';
     // import './App.css';
 
